b6191b6e8bc85abdb101326b6f5d4c2b0a069836
Lemaitre Enzo <lemaitreenzo05@gmail.com>
2025-03-10 15:53:51 +0100

    Update

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,30 @@ import './bootstrap.js';
  */
 import './styles/app.css';
 
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.mySwiper', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper2 = new Swiper('.mySwiper', {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  });
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
